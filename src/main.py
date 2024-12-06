130c3a4dfa917704b62ea149085175e813ee61aa
SmallTeddy <zhangzeming1212@gmail.com>
2024-12-06 13:32:01 +0800

    feat: 增加新的资源爬虫

--- src/main.py
+++ src/main.py
@@ -1,6 +1,10 @@
 import requests
 import time
 import json
+import base64
+import pytesseract
+from PIL import Image
+from io import BytesIO
 
 # 获取用户输入
 name = input("请输入要搜索的名称: ")
@@ -73,6 +77,42 @@ def getSearchX():
     response = requests.post('http://www.kkkob.com/v/api/getSearchX', cookies=cookies, headers=headers, data=data, verify=False)
     appendList(response)
 
+def getSearchUpYunSo():
+    # 发送请求获取数据
+    response = requests.get(f'https://upapi.juapp9.com/search?keyword={name}&page=1')
+
+    # 解码 base64 数据
+    image_data = base64.b64decode(response.text)
+    
+    # 将解码后的数据转换为字典
+    image_json_data = json.loads(image_data)
+    
+    if image_json_data['status'] == 'failed':
+        print(image_json_data['msg'])
+        return
+    
+    # 获取 code 字段的值
+    image_code_url = image_json_data["result"]["code"]
+    
+    image_response = requests.get(image_code_url)
+
+    # 将解码后的数据转换为图像
+    image = Image.open(BytesIO(image_response.content))
+    image.save('output_image.png')  # 保存图像到文件
+
+    # 使用 Tesseract 识别图像中的文本
+    captcha_text = pytesseract.image_to_string(image)
+    # print(f'识别的验证码: {captcha_text.strip()}')
+    
+    with_code_response = requests.get(f"https://upapi.juapp9.com/search?keyword={name}&page=1&code={captcha_text.strip()}")
+
+    with_code_data = base64.b64decode(with_code_response.text)
+    with_code_json_data = json.loads(with_code_data)
+    
+    final_data_list = with_code_json_data['result']['items'][4:]
+    
+    print(final_data_list)
+
 processed_data = []
 
 def showFinalList():
@@ -97,5 +137,6 @@ getJuzi()
 getXiaoyu()
 getSearchX()
 
-showFinalList()
+# getSearchUpYunSo()
 
+showFinalList()
